74c7eb682480d4f067ebf6ae9848c2b22814b0b3
nerdinand <nerdinand@nerdinand.com>
2015-08-15 14:26:06 +0200

    build with newer Android Studio version

--- app/src/main/java/com/nerdinand/eventphonecontactsmanager/MainActivity.java
+++ app/src/main/java/com/nerdinand/eventphonecontactsmanager/MainActivity.java
@@ -19,11 +19,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
